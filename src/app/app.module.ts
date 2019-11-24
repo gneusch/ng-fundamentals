02e624c6c056d3d8b75d0e67a2412bb9160a6a4d
gneusch <gneusch@gmail.com>
2019-11-24 22:26:18 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBar } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent, 
+    EventsListComponent,
+    EventThumbnailComponent, 
+    NavBar
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
